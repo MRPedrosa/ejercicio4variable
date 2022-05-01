d29517f4161783c5944fa0116e095d1575fdc567
MRPedrosa <104232967+MRPedrosa@users.noreply.github.com>
2022-04-30 22:26:50 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+let btnEnv = document.getElementById("btnEnviar");
+let btnEnv = document.getElementById("btnEnviar2");
+let base = document.forms[0].base;
+let altura = document.forms[0].altura;
+let rotulo = document.getElementById("rotulo");
+
+btnEnv.addEventListener("click", () => {
+  // TU CODIGO VA AQUI, POR EJEMPLO
+  console.log("El area es", (base * altura);
+});
+rotulo.innerHTML = "Indique la base y la altura ";
